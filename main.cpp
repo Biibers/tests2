3854b750ec33c0e7c71b2a140bf1278bdaea0f15
Biibers <77690459+Biibers@users.noreply.github.com>
2021-01-20 10:52:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 int saskaiti (int a,int b)
 {
-    int c=a+b;
-    return c;
+    return a+b;
+}
+int atnem (int a,int b)
+{
+    return a-b;
+}
+int reizini (int a,int b)
+{
+    return a*b;
+}
+int dali (int a,int b)
+{
+    return a/b;
 }
 int main()
 {
@@ -13,11 +24,23 @@ int a;
 char x;
 int b;
 cin>>a>>x>>b;
-
+int atb=0;
 if(x='+')
 {
-    cout<<saskaiti(a,b);
-
+    atb=saskaiti(a,b);
 }
+if(x='-')
+{
+    atb=atnem(a,b)
+}
+if(x='*')
+{
+    atb=reizini(a,b)
+}
+if(x='/')
+{
+    atb=dali(a,b)
+}
+cout<<atb;
 return 0;
 }
