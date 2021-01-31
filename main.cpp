543a55e18e86a1e7ee15300725b396f34964249d
Biibers <77690459+Biibers@users.noreply.github.com>
2021-01-31 18:00:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,29 +18,38 @@ int dali (int a,int b)
 {
     return a/b;
 }
+
+
 int main()
 {
 int a;
+cin>>a;
 char x;
 int b;
-cin>>a>>x>>b;
 int atb=0;
-if(x='+')
+while(cin>>x>>b&&x!='e')
 {
-    atb=saskaiti(a,b);
+    if(x=='+')
+    {
+        atb=saskaiti(a,b);
+        cout<<a<<'+'<<b<<'='<<atb;
+    }
+    else if(x=='-')
+    {
+        atb=atnem(a,b);
+        cout<<a<<'-'<<b<<'='<<atb;
+    }
+    else if(x=='*')
+    {
+        atb=reizini(a,b);
+        cout<<a<<'*'<<b<<'='<<atb;
+    }
+    else if(x=='/')
+    {
+        atb=dali(a,b);
+        cout<<a<<'/'<<b<<'='<<atb;
+    }
+    a=atb;
 }
-if(x='-')
-{
-    atb=atnem(a,b)
-}
-if(x='*')
-{
-    atb=reizini(a,b)
-}
-if(x='/')
-{
-    atb=dali(a,b)
-}
-cout<<atb;
 return 0;
 }
